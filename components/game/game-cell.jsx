6e3acff1931bb50e97f6c0422a4ca91ff08518d1
ruewith <ruewith@gmail.com>
2025-01-20 19:46:13 +0300

    Установка Next.js. Декомпозиция компонентов

---
+++ components/game/game-cell.jsx
@@ -0,0 +1,12 @@
+import { GameSymbol } from './game-symbol';
+import styles from './game.module.css';
+
+export function GameCell({ isWinner, onClick, symbol }) {
+  return (
+    <button
+      className={`${styles['cell']} ${isWinner ? styles['cell--win'] : ''}`}
+      onClick={onClick}>
+      {symbol ? <GameSymbol symbol={symbol} /> : null}
+    </button>
+  );
+}
